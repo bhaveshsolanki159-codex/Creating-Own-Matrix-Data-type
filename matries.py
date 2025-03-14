93faa9b284182c822aa7859060c3582c794ab38e
bhaveshsolanki159-codex <bhaveshsolankibj159@gmail.com>
2025-03-14 13:24:26 +0530

    matries created

--- matries.py
+++ matries.py
@@ -12,17 +12,19 @@ class matrix:
         choice = int(input())
 
         if choice == 1:
-            no_of_matrix = int(input("Enter the no of matries: "))
-            for i in range(no_of_matrix):
-                self.create_matrix()
+            self.create_matrix(choice)
             self.addition()
         elif choice == 2:
+            self.create_matrix(choice)
             self.subtraction()
         elif choice == 3:
+            self.create_matrix(choice)
             self.multiplication()
         elif choice == 4:
+            self.create_matrix(choice)
             self.division()
         elif choice == 5:
+            self.create_single_matrix()
             self.transpose()
         elif choice == 6:
             self.determinant()
@@ -39,21 +41,73 @@ class matrix:
             print("Invalid choice!")
             self.choose_operation()
     
-    def create_matrix(self):
-        self.row = int(input("Enter the number of rows: "))
-        self.col = int(input("Enter the number of columns: "))
-        for i in range(self.row):
-            a = []
-            for j in range(self.col):
-                a.append(int(input()))
-            self.matrix.append(a)
-        print(self.matrix)
+    def create_matrix(self, choice):
+        try:
+            no_of_matrix = int(input("Enter the number of matrices: "))
+            for x in range(no_of_matrix):
+                self.row = int(input("Enter the number of rows: "))  # n
+                self.col = int(input("Enter the number of columns: "))  # m
+                sample = []
+                for i in range(self.row):
+                    a = []
+                    for j in range(self.col):
+                        a.append(int(input(f"Enter element [{i}][{j}]: ")))
+                    sample.append(a)
+                self.matrix.append(sample)
+            print(self.matrix)
+        except ValueError:
+            print("Invalid input! Please enter integers only.")
+            self.create_matrix(choice)
+                
+    def create_single_matrix(self):
+        try:
+            self.row = int(input("Enter the number of rows: "))  # n
+            self.col = int(input("Enter the number of columns: "))  # m
+            for i in range(self.row):
+                a = []
+                for j in range(self.col):
+                    a.append(int(input(f"Enter element [{i}][{j}]: ")))
+                self.matrix.append(a)
+            print(self.matrix)
+        except ValueError:
+            print("Invalid input! Please enter integers only.")
+            self.create_single_matrix()
     
     def addition(self):
+        # Implement addition logic here
         pass
 
-        
+    def subtraction(self):
+        # Implement subtraction logic here
+        pass
+
+    def multiplication(self):
+        # Implement multiplication logic here
+        pass
+
+    def division(self):
+        # Implement division logic here
+        pass
+
+    def transpose(self):
+        # Implement transpose logic here
+        pass
+
+    def determinant(self):
+        # Implement determinant logic here
+        pass
+
+    def inverse(self):
+        # Implement inverse logic here
+        pass
+
+    def rank(self):
+        # Implement rank logic here
+        pass
+
+    def power(self):
+        # Implement power logic here
+        pass
 
 
 X = matrix()
-
